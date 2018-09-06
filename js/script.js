8691800d27a8c0e8fd1a5517e578bec37d84b444
Alexandr Ciganthuk <MyAlexEducation@gmail.com>
2018-09-06 16:58:36 +0300

    Анимация всплытия модалки

--- js/script.js
+++ js/script.js
@@ -11,7 +11,7 @@ promotion_btns[0].addEventListener("click", function(evt) {
   promotion_slids[0].classList.add("current");
   promotion_slide_current.classList.remove("current");
   promotion_slide_current = promotion_slids[0];
-})
+});
 promotion_btns[1].addEventListener("click", function(evt) {
   evt.preventDefault();
   promotion_btns[1].classList.add("current");
@@ -20,7 +20,7 @@ promotion_btns[1].addEventListener("click", function(evt) {
   promotion_slids[1].classList.add("current");
   promotion_slide_current.classList.remove("current");
   promotion_slide_current = promotion_slids[1];
-})
+});
 promotion_btns[2].addEventListener("click", function(evt) {
   evt.preventDefault();
   promotion_btns[2].classList.add("current");
@@ -29,7 +29,7 @@ promotion_btns[2].addEventListener("click", function(evt) {
   promotion_slids[2].classList.add("current");
   promotion_slide_current.classList.remove("current");
   promotion_slide_current = promotion_slids[2];
-})
+});
 //////////////////////////////////////////////////////////////////////
 var service_slids = document.querySelectorAll(".service-item");
 var service_slide_current = document.querySelector(".service-item.current");
@@ -43,7 +43,7 @@ service_btns[0].addEventListener("click", function(evt) {
   service_slids[0].classList.add("current");
   service_slide_current.classList.remove("current");
   service_slide_current = service_slids[0];
-})
+});
 service_btns[1].addEventListener("click", function(evt) {
   evt.preventDefault();
   service_btns[1].classList.add("current");
@@ -52,7 +52,7 @@ service_btns[1].addEventListener("click", function(evt) {
   service_slids[1].classList.add("current");
   service_slide_current.classList.remove("current");
   service_slide_current = service_slids[1];
-})
+});
 service_btns[2].addEventListener("click", function(evt) {
   evt.preventDefault();
   service_btns[2].classList.add("current");
@@ -61,25 +61,25 @@ service_btns[2].addEventListener("click", function(evt) {
   service_slids[2].classList.add("current");
   service_slide_current.classList.remove("current");
   service_slide_current = service_slids[2];
-})
-//Оживление модалок
-var about_us_btn = document.querySelector(".about-us .btn");
-var about_us_modal = document.querySelector(".modal-feedback");
-about_us_btn.addEventListener("click", function(evt){
-  evt.preventDefault();
-  about_us_modal.classList.add("modal-show");
-})
-var close_about_us_modal = about_us_modal.querySelector(".modal-close");
-close_about_us_modal.addEventListener("click", function(evt){
-  evt.preventDefault();
-  about_us_modal.classList.remove("modal-show");
 });
-var about_us_modal_form = about_us_modal.querySelector("form");
-var about_us_modal_form_name = about_us_modal_form.querySelector("[name=name]");
-var about_us_modal_form_email = about_us_modal_form.querySelector("[name=email]");
-var about_us_modal_form_letter = about_us_modal_form.querySelector("[name=letter]");
-about_us_modal_form.addEventListener("submit", function(evt){
-  if(!about_us_modal_form_name.value || !about_us_modal_form_email.value || !about_us_modal_form_letter.value) {
+//Оживление модалок
+var contacts_btn = document.querySelector(".contacts .btn");
+var contacts_modal = document.querySelector(".modal-feedback");
+contacts_btn.addEventListener("click", function(evt){
+  evt.preventDefault();
+  contacts_modal.classList.add("modal-show");
+});
+var close_contacts_modal = contacts_modal.querySelector(".modal-close");
+close_contacts_modal.addEventListener("click", function(evt){
+  evt.preventDefault();
+  contacts_modal.classList.remove("modal-show");
+});
+var contacts_modal_form = contacts_modal.querySelector("form");
+var contacts_modal_form_name = contacts_modal_form.querySelector("[name=name]");
+var contacts_modal_form_email = contacts_modal_form.querySelector("[name=email]");
+var contacts_modal_form_letter = contacts_modal_form.querySelector("[name=letter]");
+contacts_modal_form.addEventListener("submit", function(evt){
+  if(!contacts_modal_form_name.value || !contacts_modal_form_email.value || !contacts_modal_form_letter.value) {
        evt.preventDefault();
      }
 });
@@ -89,9 +89,9 @@ var modal_map = document.querySelector(".modal-map");
 contacts_map.addEventListener("click",function(evt){
   evt.preventDefault();
   modal_map.classList.add("modal-show");
-})
+});
 var close_modal_map = modal_map.querySelector(".modal-close");
 close_modal_map.addEventListener("click", function(evt){
   evt.preventDefault();
   modal_map.classList.remove("modal-show");
-})
+});
